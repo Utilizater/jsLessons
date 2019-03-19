a2b2b6d6e8b6b23fd375c42e37c8f4c3d49ba487
Nikita <polevoynikita@yandex.ru>
2019-03-19 15:58:19 +0100

    lesson12

--- project/js/script.js
+++ project/js/script.js
@@ -225,85 +225,97 @@ window.addEventListener('DOMContentLoaded', () => {
         event.preventDefault();
         form.appendChild(statusMesaage);
 
-        let requesst = new XMLHttpRequest();
-        requesst.open('POST', 'server.php');
-        requesst.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
         let formData = new FormData(form);
-
+        
         let obj = {};
         formData.forEach((value, key) => {
           obj[key] = value;
         });
-
         var json = JSON.stringify(obj);
 
-        requesst.send(json);
-        requesst.addEventListener('readystatechange', () => {
-          if (requesst.readyState < 4) {
-            statusMesaage.innerHTML = message.loading;
-          } else if (requesst.readyState == 4 && requesst.status == 200) {
-            statusMesaage.innerHTML = "";
-            statusMesaage.style.height = "80px";
-            statusMesaage.style.background =  `url(${message.success}) no-repeat center`;
-            statusMesaage.style.marginTop = "10px";
-            } else {
+
+        function postData(data) {
+          return new Promise(function(resolve, reject){
+            let requesst = new XMLHttpRequest();
+            requesst.open('POST', 'server.php');
+            requesst.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+            
+            requesst.addEventListener('readystatechange', () => {
+              if (requesst.readyState < 4) {
+                statusMesaage.innerHTML = message.loading;
+              } else if (requesst.readyState == 4 && requesst.status == 200) {
+                statusMesaage.innerHTML = "";
+                statusMesaage.style.height = "80px";
+                statusMesaage.style.background =  `url(${message.success}) no-repeat center`;
+                statusMesaage.style.marginTop = "10px";
+                } else {
+                  statusMesaage.innerHTML = "";
+                  statusMesaage.style.height = "80px";
+                  statusMesaage.style.background =  `url(${message.failure}) no-repeat center`;
+                  statusMesaage.style.marginTop = "10px";
+              }
+            });
+            requesst.send(data);
+          });
+        }
+
+        postData(formData)
+              .then(() => {
+                statusMesaage.innerHTML = message.loading;
+              })
+              .then(() => {
+                statusMesaage.innerHTML = "";
+                statusMesaage.style.height = "80px";
+                statusMesaage.style.background =  `url(${message.success}) no-repeat center`;
+                statusMesaage.style.marginTop = "10px";
+              })
+              .catch(() => {
               statusMesaage.innerHTML = "";
               statusMesaage.style.height = "80px";
               statusMesaage.style.background =  `url(${message.failure}) no-repeat center`;
-              statusMesaage.style.marginTop = "10px";
-          }
-        });
-
-        for (let i = 0; i < input.length; i++) {
-          input[i].value = "";
-        }
-
+              statusMesaage.style.marginTop = "10px";})
+              .then(() => {
+                for (let i = 0; i < input.length; i++) {
+                  input[i].value = "";
+                }})
+        
       });
       //second form
       let secondForm = document.getElementById("form"),
-        secondFormInputs = secondForm.getElementsByTagName("input");
-      secondForm.addEventListener('submit', (event) => {
+          secondFormInputs = secondForm.getElementsByTagName("input");
+          secondForm.addEventListener('submit', (event) => {
           event.preventDefault();
 
-
-          console.log(phonenumber(secondFormInputs[1].value));
           secondForm.appendChild(statusMesaage);
 
-          let requesst = new XMLHttpRequest();
-          requesst.open('POST', 'server.php');
-          requesst.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
           let formData = new FormData(secondForm);
-
           let obj = {};
           formData.forEach((value, key) => {
             obj[key] = value;
           });
-
           var json = JSON.stringify(obj);
+          
+          function postData(data) {
+            return new Promise(function(resolve, reject){
+              let requesst = new XMLHttpRequest();
+              requesst.open('POST', 'server.php');
+              requesst.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
-          requesst.send(json);
-          requesst.addEventListener('readystatechange', () => {
-            if (requesst.readyState < 4) {
-              statusMesaage.innerHTML = message.loading;
-            } else if (requesst.readyState == 4 && requesst.status == 200) {
-              statusMesaage.innerHTML = "";
-            statusMesaage.style.height = "80px";
-            statusMesaage.style.background =  `url(${message.success}) no-repeat center`;
-            statusMesaage.style.marginTop = "10px";
-            } else {
-              statusMesaage.innerHTML = "";
-              statusMesaage.style.height = "80px";
-              statusMesaage.style.background =  `url(${message.failure}) no-repeat center`;
-              statusMesaage.style.marginTop = "10px";
-            }
-          });
-
-          for (let i = 0; i < input.length; i++) {
-            input[i].value = "";
+              requesst.addEventListener('readystatechange', () => {
+                if (requesst.readyState < 4) {
+                  statusMesaage.innerHTML = message.loading;
+                } else if (requesst.readyState == 4 && requesst.status == 200) {
+                resolve();
+                } else {
+                  reject();
+                }
+                
+              });
+              requesst.send(data); 
+            });
           }
-
+          
           function phonenumber(inputtxt) {
             var phoneno = /^[\+]\d{10}$/;
             if (inputtxt.match(phoneno)) {
@@ -313,7 +325,25 @@ window.addEventListener('DOMContentLoaded', () => {
                 return false;
               }
             }
+
+            postData(formData)
+              .then(() => {
+                statusMesaage.innerHTML = message.loading;
+              })
+              .then(() => {
+                statusMesaage.innerHTML = "";
+                statusMesaage.style.height = "80px";
+                statusMesaage.style.background =  `url(${message.success}) no-repeat center`;
+                statusMesaage.style.marginTop = "10px";
+              })
+              .catch(() => {
+              statusMesaage.innerHTML = "";
+              statusMesaage.style.height = "80px";
+              statusMesaage.style.background =  `url(${message.failure}) no-repeat center`;
+              statusMesaage.style.marginTop = "10px";})
+              .then(() => {
+                for (let i = 0; i < secondFormInputs.length; i++) {
+                  secondFormInputs[i].value = "";
+                }})
           });
-
-
-      });
+});
